84f0823cbd7fe495a8c1f3d915910c591a2ebdaf
Sarahlim4 <sarahivoneferreiralima@gmail.com>
2022-11-10 11:25:18 -0300

    second commit

---
+++ function.js
@@ -0,0 +1,17 @@
+const x = 5;
+const y = 7;
+const z = 9;
+
+function digaOla(){
+    console.log('Olá');
+}
+
+function multiplica(a, b, t) {
+    return a*b*t;
+
+}
+
+digaOla();
+const resultado = multiplica(y ,x, z);
+
+console.log(resultado)
--- script.js
+++ script.js
@@ -2,17 +2,31 @@ const toDoField = document.querySelector('.to-do-field');
 const toDoSend = document.querySelector('.to-do-send');
 const toDolist = document.querySelector('.to-do-list');
 
-toDoSend.addEventListener('click', function(e){
-    console.log(toDoField.value);
+function createList() {
     const li = document.createElement('li');
-    const buttonClear = document.createElement ('button');
+    const buttonClear = document.createElement('button')
     buttonClear.setAttribute('class', 'button-clear');
     buttonClear.innerText = 'Apagar';
     li.innerText = toDoField.value;
-    li.appendChild(buttonClear);
+    li.appendChild(buttonClear)
     toDolist.appendChild(li);
+    return li;
+
+}
+
+toDoSend.addEventListener('click', function(e){
+    createList()
 })
-   
+
+
+
+addEventListener('keyup', (e) => {
+    console.log(e.key)
+    if (e.key == 'Enter') {
+        createList();
+    }
+})
+
 addEventListener('click', function(e){
     const el = e.target;
 
